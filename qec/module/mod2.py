6224e285c4adde4a387aa4c820c2ff8226383afa
caohanyan <caohanyan@itp.ac.cn>
2023-10-07 23:34:54 +0800

    new

--- qec/module/mod2.py
+++ qec/module/mod2.py
@@ -1,5 +1,6 @@
 import torch
 from copy import deepcopy
+from ldpc.mod2 import row_echelon, row_basis, rank
 #from utils import exact_config
 
 def row_sum_sort(t):
@@ -129,134 +130,130 @@ class mod2():
         gamma = torch.cat([A, B], dim=1)
         return ((bin_a @ gamma @ bin_b.T)%2).squeeze()
 
-    def row_echelon(self, M, b=None):
+    def echelon(self, M, b=None):
         
-        M = deepcopy(M).to(self.dtype).to(self.device) 
-        if b != None:
-            b = deepcopy(b).to(self.dtype).to(self.device)
-        '''Gauss elimination'''
-        M1 = 0
-        while (M-M1).sum() != 0:
-            M1 = deepcopy(M)
-            #print(M)
-            for i in range(M.size(0)):
-                if M[i].sum() == 0:
-                    for j in range(i, M.size(0)):
-                        if M[j].sum() !=0:
-                            M[[i, j]] = M[[j, i]]
-                    if b != None:
-                        b[[i, j]] = b[[j, i]]
-            for i in range(min(M.size(0), M.size(1))):
-                if M[i,i] == 1:
-                    None
-                else:
-                    for j in range(i, M.size(0)):
-                                if M[j, i] == 1:
-                                    M[[i, j]] = M[[j, i]]
-                                    if b != None:
-                                        b[[i, j]] = b[[j, i]]
-            #print(M)
-            c_idx = []
-            for i in range(min(M.size(0), M.size(1))):
-                if M[i,i] == 1:
-                    c_idx.append(i)
-                else:
-                    if M[i].sum() !=0:
-                        a = torch.nonzero(M[i])[0].item()
-                        if a>i:
-                            c_idx.append(a)
-                    
-           # print(c_idx)
-            for i in range(min(M.size(0), M.size(1))):
-                for j in range(len(c_idx)):
-                    idx = c_idx[j]
-                    #print(i, j, idx)
-                    if M[i, idx] == 1 and j < i:
-                        M[i, :] = abs(M[i, :] - M[j, :])
-                        if b != None:
-                            b[i] = abs(b[i] - b[j])
-           
-            for i in range(M.size(0)):
-                if M[i].sum() == 0:
-                    for j in range(i, M.size(0)):
-                        if M[j].sum() !=0:
-                            M[[i, j]] = M[[j, i]]
-                    if b != None:
-                        b[[i, j]] = b[[j, i]]
-            
-            for i in range(min(M.size(0), M.size(1))):
-                if M[i,i] == 1:
-                    None
-                else:
-                    for j in range(i, M.size(0)):
-                        if M[j, i] == 1:
-                            M[[i, j]] = M[[j, i]]
-                            if b != None:
-                                b[[i, j]] = b[[j, i]]
-            for i in range(M.size(0)):
-                for j in range(i+1, min(M.size(0), M.size(1))):
-                    if M[i, j] == 1 and M[j, j] == 1:
-                        M[i, :] = abs(M[i, :]- M[j, :])
-                        if b != None:
-                            b[i] = abs(b[i] - b[j])
-            
-        for i in range(M.size(0)):
-            for j in range(M.size(0)):
-                if j!= i and abs(M[i]-M[j]).sum()==0: 
-                    M[j] = 0
-                    if b != None:
-                        b[j] = 0
-        for i in range(M.size(0)):
-            if M[i].sum() == 0:
-                for j in range(i, M.size(0)):
-                    if M[j].sum() !=0:
-                        M[[i, j]] = M[[j, i]]
-                if b != None:
-                    b[[i, j]] = b[[j, i]]
-
-        for i in range(min(M.size(0), M.size(1))):
-            if M[i,i] == 1:
-                None
-            else:
-                for j in range(i, M.size(0)):
-                    if M[j, i] == 1:
-                        M[[i, j]] = M[[j, i]]
-                        if b != None:
-                            b[[i, j]] = b[[j, i]]
-        
-        #print(M)
-        if b != None:
-            return M, b
+        if b!= None:
+            if b.dim() == 1:
+                b = b.unsqueeze(1)
+            X = torch.hstack([M, b])
         else:
-            return M
+            X = M
+        X = X.cpu().numpy()
+        re = row_echelon(X)[0]
+        #print(re)
+        re = torch.from_numpy(re)
+        # M = deepcopy(M).to(self.dtype).to(self.device) 
+        # if b != None:
+        #     b = deepcopy(b).to(self.dtype).to(self.device)
+        # '''Gauss elimination'''
+        # M1 = 0
+        # while (M-M1).sum() != 0:
+        #     M1 = deepcopy(M)
+        #     #print(M)
+        #     for i in range(M.size(0)):
+        #         if M[i].sum() == 0:
+        #             for j in range(i, M.size(0)):
+        #                 if M[j].sum() !=0:
+        #                     M[[i, j]] = M[[j, i]]
+        #             if b != None:
+        #                 b[[i, j]] = b[[j, i]]
+        #     for i in range(min(M.size(0), M.size(1))):
+        #         if M[i,i] == 1:
+        #             None
+        #         else:
+        #             for j in range(i, M.size(0)):
+        #                         if M[j, i] == 1:
+        #                             M[[i, j]] = M[[j, i]]
+        #                             if b != None:
+        #                                 b[[i, j]] = b[[j, i]]
+        #     #print(M)
+        #     c_idx = []
+        #     for i in range(min(M.size(0), M.size(1))):
+        #         if M[i,i] == 1:
+        #             c_idx.append(i)
+        #         else:
+        #             if M[i].sum() !=0:
+        #                 a = torch.nonzero(M[i])[0].item()
+        #                 if a>i:
+        #                     c_idx.append(a)
+                    
+        #    # print(c_idx)
+        #     for i in range(min(M.size(0), M.size(1))):
+        #         for j in range(len(c_idx)):
+        #             idx = c_idx[j]
+        #             #print(i, j, idx)
+        #             if M[i, idx] == 1 and j < i:
+        #                 M[i, :] = abs(M[i, :] - M[j, :])
+        #                 if b != None:
+        #                     b[i] = abs(b[i] - b[j])
+           
+        #     for i in range(M.size(0)):
+        #         if M[i].sum() == 0:
+        #             for j in range(i, M.size(0)):
+        #                 if M[j].sum() !=0:
+        #                     M[[i, j]] = M[[j, i]]
+        #             if b != None:
+        #                 b[[i, j]] = b[[j, i]]
+            
+        #     for i in range(min(M.size(0), M.size(1))):
+        #         if M[i,i] == 1:
+        #             None
+        #         else:
+        #             for j in range(i, M.size(0)):
+        #                 if M[j, i] == 1:
+        #                     M[[i, j]] = M[[j, i]]
+        #                     if b != None:
+        #                         b[[i, j]] = b[[j, i]]
+        #     for i in range(M.size(0)):
+        #         for j in range(i+1, min(M.size(0), M.size(1))):
+        #             if M[i, j] == 1 and M[j, j] == 1:
+        #                 M[i, :] = abs(M[i, :]- M[j, :])
+        #                 if b != None:
+        #                     b[i] = abs(b[i] - b[j])
+            
+        # for i in range(M.size(0)):
+        #     for j in range(M.size(0)):
+        #         if j!= i and abs(M[i]-M[j]).sum()==0: 
+        #             M[j] = 0
+        #             if b != None:
+        #                 b[j] = 0
+        # for i in range(M.size(0)):
+        #     if M[i].sum() == 0:
+        #         for j in range(i, M.size(0)):
+        #             if M[j].sum() !=0:
+        #                 M[[i, j]] = M[[j, i]]
+        #         if b != None:
+        #             b[[i, j]] = b[[j, i]]
+
+        # for i in range(min(M.size(0), M.size(1))):
+        #     if M[i,i] == 1:
+        #         None
+        #     else:
+        #         for j in range(i, M.size(0)):
+        #             if M[j, i] == 1:
+        #                 M[[i, j]] = M[[j, i]]
+        #                 if b != None:
+        #                     b[[i, j]] = b[[j, i]]
+        
+        # #print(M)
+        if b != None:
+            e = re[:, :-b.size(1)]
+            x = re[:, -b.size(1):].squeeze()
+            return e, x
+        else:
+            return re
 
     def rank(self, M):
-        M_row_echelon = self.row_echelon(M)
-        r = M_row_echelon.size(0)
-        for i in range(M_row_echelon.size(0)):
-            if M_row_echelon[i].sum() == 0:
-                r -= 1
+        r = rank(M.cpu().numpy())
         return r
-    def L_indep(self, M):
-        A = M[0]
-        r=1
-        for i in range(1, M.size(0)):
-            B = torch.vstack([A, M[i]])
-            r1 = self.rank(B)
-            #print(r)
-            if r1 == r+1 :
-                A = B
-                r = r1
-            else:
-                None
-            #print(A)
-        return A
+    def indep(self, M):
+        return torch.from_numpy(row_basis(M.cpu().numpy()))
             
             
             
 
     def solve(self, M, b):
-        M, b= self.row_echelon(M, b)
+        M, b= self.echelon(M, b)
         n = M.size(1)
         m = M.size(0)
         if len(b.size())>1:
@@ -340,79 +337,78 @@ class mod2():
             c+=1
         return x.squeeze()
 
-    def solution_space(self, M, b):
-        M, b = self.row_echelon(M, b)
-        #print(M)
-        n = M.size(1)
-        m = M.size(0)
-        x = torch.zeros(n, dtype=int)
+    # def solution_space(self, M, b):
+    #     M, b = self.echelon(M, b)
+    #     n = M.size(1)
+    #     m = M.size(0)
+    #     x = torch.zeros(n, dtype=int)
         
-        fv = n-m
-        vars = []
-        for i in range(n):
-            if M[:, i].sum() == 0:
-                vars.append(i)
+    #     fv = n-m
+    #     vars = []
+    #     for i in range(n):
+    #         if M[:, i].sum() == 0:
+    #             vars.append(i)
         
-        v=n
-        c=0
-        while c-v !=0 and v>0:
-            c=v
-            for i in range(m):
-                if M[i].sum() == 1:
-                    v=v-1
-                    idx = M[i].nonzero().squeeze()
-                    x[idx] = b[i]
-                    b = abs(b-(M[:, idx]*x[idx])%2)
-                    M[:, idx] = 0           
+    #     v=n
+    #     c=0
+    #     while c-v !=0 and v>0:
+    #         c=v
+    #         for i in range(m):
+    #             if M[i].sum() == 1:
+    #                 v=v-1
+    #                 idx = M[i].nonzero().squeeze()
+    #                 x[idx] = b[i]
+    #                 b = abs(b-(M[:, idx]*x[idx])%2)
+    #                 M[:, idx] = 0           
 
-        for i in range(n):
-            for j in range(i, n):
-                if j!=i and abs(M[:, j]- M[:, i]).sum() == 0 and M[:, j].sum() !=0 and j not in vars:
-                    vars.append(j)
+    #     for i in range(n):
+    #         for j in range(i, n):
+    #             if j!=i and abs(M[:, j]- M[:, i]).sum() == 0 and M[:, j].sum() !=0 and j not in vars:
+    #                 vars.append(j)
 
-        M1 = deepcopy(M)
-        while len(vars) < fv:
-            c_max = torch.argmax(torch.sum(M1, dim=0)).item()
-            if c_max not in vars:
-                vars.append(c_max)
-            M1[:, c_max] = 0
-            for i in range(m):
-                if M1[i].sum() == 1:
-                    idx = M1[i].nonzero().squeeze()
-                    M1[:, idx] = 0
+    #     M1 = deepcopy(M)
+    #     while len(vars) < fv:
+    #         c_max = torch.argmax(torch.sum(M1, dim=0)).item()
+    #         if c_max not in vars:
+    #             vars.append(c_max)
+    #         M1[:, c_max] = 0
+    #         for i in range(m):
+    #             if M1[i].sum() == 1:
+    #                 idx = M1[i].nonzero().squeeze()
+    #                 M1[:, idx] = 0
 
-        vars.sort()
+    #     vars.sort()
 
-        space = torch.vstack([x]*2**len(vars))
-        b = torch.vstack([b]*2**len(vars))
-        config = exact_config(D=len(vars), N=2**len(vars)).long()
-        #print(config)
-        for i in range(2**len(vars)):
-            space[i][vars] = config[i]
-        for j in range(m):   
-            if M[j].sum() != 0:
-                b[:, j] = (b[:, j] - (M[j, vars]@config.T)%2)%2
-                M[j, vars] = 0
+    #     space = torch.vstack([x]*2**len(vars))
+    #     b = torch.vstack([b]*2**len(vars))
+    #     config = exact_config(D=len(vars), N=2**len(vars)).long()
+    #     #print(config)
+    #     for i in range(2**len(vars)):
+    #         space[i][vars] = config[i]
+    #     for j in range(m):   
+    #         if M[j].sum() != 0:
+    #             b[:, j] = (b[:, j] - (M[j, vars]@config.T)%2)%2
+    #             M[j, vars] = 0
                 
-        v=n
-        c=0
-        while c-v !=0 :
-            c=v
-            for i in range(m):
-                if M[i].sum() == 1:
-                    v=v-1
-                    idx = M[i].nonzero().squeeze()
-                    space[:, idx] = b[:, i]
-                    b[:, i] = abs(b[:, i]-space[:, idx])
-                    for j in range(m):
-                        if M[j, idx] == 1 and j!=i:
-                            b[:, j] = abs(b[:, j]-space[:, idx])
-                    M[:, idx] = 0 
+    #     v=n
+    #     c=0
+    #     while c-v !=0 :
+    #         c=v
+    #         for i in range(m):
+    #             if M[i].sum() == 1:
+    #                 v=v-1
+    #                 idx = M[i].nonzero().squeeze()
+    #                 space[:, idx] = b[:, i]
+    #                 b[:, i] = abs(b[:, i]-space[:, idx])
+    #                 for j in range(m):
+    #                     if M[j, idx] == 1 and j!=i:
+    #                         b[:, j] = abs(b[:, j]-space[:, idx])
+    #                 M[:, idx] = 0 
                  
-        return space
+    #     return space
     
     def kernel(self, M):
-        M = self.row_echelon(M)
+        M = self.echelon(M)
         #print(M)
         n = M.size(1)
         m = M.size(0)
@@ -562,11 +558,11 @@ if __name__ == '__main__':
         while m2.rank(M) != m:
             M = torch.randint(0, 2, (m, n))
         return M
-    m, n = 5, 6
+    m, n = 36, 40
     M = Matrix(m, n)
-    print(M)
+    print(m2.rank(M))
     print('row echelon M :')
-    print(m2.row_echelon(M))
+    print(m2.echelon(M))
 
     print('free var number :', n-m)
 
@@ -574,20 +570,22 @@ if __name__ == '__main__':
     print('solution :')
     x0 = m2.solve(M, b1)
     print(x0)
-    print((M@x0.T)%2)
+    print('check:', (abs(M@x0.T)%2 - torch.eye(m)).sum().item() == 0)
 
-    print('initial a result vector b:')
-    b = torch.ones(m).long()
-    print(b)
+    # print('initial a result vector b:')
+    # b = torch.ones(m).long()
+    # print(b)
     
-    x = m2.solution_space(M, b)
-    print('solution space :')
-    print(x)
-    print('test!!!')
-    print((M@x.T)%2)
-    print('space rank :', m2.rank(x))
+    # x = m2.solution_space(M, b)
+    # print('solution space :')
+    # print(x)
+    # print('test!!!')
+    # print((M@x.T)%2)
+    # print('space rank :', m2.rank(x))
 
-
+    k = m2.kernel(M)
+    kdim = m2.rank(k)
+    print('kernel dim:', m2.rank(k), kdim-n+m==0)
     
     
     # A = torch.tensor([[1, 0, 1, 0, 0, 0, 0, 0],
